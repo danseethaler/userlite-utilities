276bff3a592164ed09c987f1d9bdb9b27f77d1a7
Dan Seethaler <dan@seethalers.com>
2015-11-21 21:22:45 -0700

    Initial commit.

---
+++ spec/userlite-utilities-view-spec.js
@@ -0,0 +1,9 @@
+var UserliteUtilitiesView;
+
+UserliteUtilitiesView = require('../lib/userlite-utilities-view');
+
+describe("UserliteUtilitiesView", function() {
+    return it("has one valid test", function() {
+        return expect("life").toBe("easy");
+    });
+});
